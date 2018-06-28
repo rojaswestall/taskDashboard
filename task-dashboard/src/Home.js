120118dc6ab4173db606cbe86c46d410a7d3eb90
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-06-27 18:29:47 -0700

    removed top nav bar

--- task-dashboard/src/Home.js
+++ task-dashboard/src/Home.js
@@ -1,67 +1,72 @@
 import React, { Component } from 'react';
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
-const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu;
 
 class Home extends Component {
-	render() {
-		return (
-			<Layout>
-			    <Header className="header">
-			      <div className="logo" />
-			      <Menu
-			        theme="dark"
-			        mode="horizontal"
-			        defaultSelectedKeys={['2']}
-			        style={{ lineHeight: '64px' }}
-			      >
-			        <Menu.Item key="1">nav 1</Menu.Item>
-			        <Menu.Item key="2">nav 2</Menu.Item>
-			        <Menu.Item key="3">nav 3</Menu.Item>
-			      </Menu>
-			    </Header>
-			    <Layout>
-			      <Sider width={200} style={{ background: '#fff' }}>
-			        <Menu
-			          mode="inline"
-			          defaultSelectedKeys={['1']}
-			          defaultOpenKeys={['sub1']}
-			          style={{ height: '100%', borderRight: 0 }}
-			        >
-			          <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
-			            <Menu.Item key="1">option1</Menu.Item>
-			            <Menu.Item key="2">option2</Menu.Item>
-			            <Menu.Item key="3">option3</Menu.Item>
-			            <Menu.Item key="4">option4</Menu.Item>
-			          </SubMenu>
-			          <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-			            <Menu.Item key="5">option5</Menu.Item>
-			            <Menu.Item key="6">option6</Menu.Item>
-			            <Menu.Item key="7">option7</Menu.Item>
-			            <Menu.Item key="8">option8</Menu.Item>
-			          </SubMenu>
-			          <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-			            <Menu.Item key="9">option9</Menu.Item>
-			            <Menu.Item key="10">option10</Menu.Item>
-			            <Menu.Item key="11">option11</Menu.Item>
-			            <Menu.Item key="12">option12</Menu.Item>
-			          </SubMenu>
-			        </Menu>
-			      </Sider>
-			      <Layout style={{ padding: '0 24px 24px' }}>
-			        <Breadcrumb style={{ margin: '16px 0' }}>
-			          <Breadcrumb.Item>Home</Breadcrumb.Item>
-			          <Breadcrumb.Item>List</Breadcrumb.Item>
-			          <Breadcrumb.Item>App</Breadcrumb.Item>
-			        </Breadcrumb>
-			        <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
-			          Content
-			        </Content>
-			      </Layout>
-			    </Layout>
-			</Layout>
-  		);
-	}
+	 state = {
+	    collapsed: false,
+	  };
+	  onCollapse = (collapsed) => {
+	    console.log(collapsed);
+	    this.setState({ collapsed });
+	  }
+	  render() {
+	    return (
+	      <Layout style={{ minHeight: '100vh' }}>
+	        <Sider
+	          collapsible
+	          collapsed={this.state.collapsed}
+	          onCollapse={this.onCollapse}
+	        >
+	          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+	          	<Menu.Item key="1">
+	              <Icon type="home" />
+	              <span>Home</span>
+	            </Menu.Item>
+	          	<Menu.Item key="2">
+	              <Icon type="book" />
+	              <span>Study Hours</span>
+	            </Menu.Item>
+	            <Menu.Item key="3">
+	              <Icon type="heart" />
+	              <span>Service</span>
+	            </Menu.Item>
+	            <Menu.Item key="4">
+	              <Icon type="pie-chart" />
+	              <span>Metrics</span>
+	            </Menu.Item>
+	            
+	            <SubMenu
+	              key="sub1"
+	              title={<span><Icon type="team" /><span>Team</span></span>}
+	            >
+	              <Menu.Item key="6">Guajiro</Menu.Item>
+	              <Menu.Item key="7">Vagabundo</Menu.Item>
+	              <Menu.Item key="8">Canis</Menu.Item>
+	              <Menu.Item key="9">Marchitar</Menu.Item>
+	            </SubMenu>
+
+	          </Menu>
+	        </Sider>
+	        <Layout>
+	        	<Header style={{ background: '#fff', padding: 0 }} />
+	          <Content style={{ margin: '0 16px' }}>
+	            <Breadcrumb style={{ margin: '16px 0' }}>
+	              <Breadcrumb.Item>Team</Breadcrumb.Item>
+	              <Breadcrumb.Item>Guajiro</Breadcrumb.Item>
+	            </Breadcrumb>
+	            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+	              Stuff about tasks, service, and study hours
+	            </div>
+	          </Content>
+	          <Footer style={{ textAlign: 'center' }}>
+	            Made with <Icon type="heart"/> by Gabe Rojas-Westall © 2018
+	          </Footer>
+	        </Layout>
+	      </Layout>
+	    );
+	  }
 }
 
 export default Home;
